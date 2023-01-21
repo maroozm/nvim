2006e2b4df83fb4d77196de19bb3955b1b56cdfb
Salman KMalik <salman.khurshid.malik@cern.ch>
2023-01-21 23:07:56 +0530

    my nvim config

---
+++ lua/plugins.lua
@@ -0,0 +1,36 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+--themes
+Plug "habamax/vim-saturnite"
+Plug "lunarvim/darkplus.nvim"
+Plug "rmehri01/onenord.nvim"
+Plug "folke/tokyonight.nvim"
+
+--misc
+Plug "junegunn/vim-plug"
+Plug "lewis6991/impatient.nvim"
+Plug "tpope/vim-commentary"
+Plug "karb94/neoscroll.nvim"
+Plug "nvim-treesitter/nvim-treesitter"
+
+--gitsigns
+Plug "lewis6991/gitsigns.nvim"
+
+--completions
+Plug "hrsh7th/nvim-cmp"
+Plug "hrsh7th/cmp-buffer"
+Plug "hrsh7th/cmp-path"
+Plug "saadparwaiz1/cmp_luasnip"
+Plug "hrsh7th/cmp-nvim-lua"
+
+--comment
+Plug 'numToStr/Comment.nvim'
+
+--fuzzyfinder
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = vim.fn['make']})
+
+vim.call('plug#end')
