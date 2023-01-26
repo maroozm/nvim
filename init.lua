7102c1b835811507b6e3e9aacdd6bf9857b1b485
Salman KMalik <salman.khurshid.malik@cern.ch>
2023-01-26 12:04:08 +0530

    config

--- init.lua
+++ init.lua
@@ -43,9 +43,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.fillchars.eob=" "
 -- use space as leader key
 vim.g.mapleader = " "
-
 vim.opt.shortmess:append "c"
-
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
@@ -53,41 +51,33 @@ end
 -- Plugins
 -- -------
 local Plug = vim.fn['plug#']
-
 vim.call('plug#begin', '~/.config/nvim/plugged')
-
 --themes
 Plug "habamax/vim-saturnite"
 Plug "lunarvim/darkplus.nvim"
 Plug "rmehri01/onenord.nvim"
 Plug "folke/tokyonight.nvim"
 Plug "getomni/neovim"
-
 --misc
 Plug "junegunn/vim-plug"
 Plug "lewis6991/impatient.nvim"
 Plug "tpope/vim-commentary"
 Plug "karb94/neoscroll.nvim"
 Plug "nvim-treesitter/nvim-treesitter"
-
 --gitsigns
 Plug "lewis6991/gitsigns.nvim"
-
 --completions
 Plug "hrsh7th/nvim-cmp"
 Plug "hrsh7th/cmp-buffer"
 Plug "hrsh7th/cmp-path"
 Plug "saadparwaiz1/cmp_luasnip"
 Plug "hrsh7th/cmp-nvim-lua"
-
 --comment
 Plug 'numToStr/Comment.nvim'
-
 --fuzzyfinder
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = vim.fn['make']})
-
 vim.call('plug#end')
 
 -- Colorscheme
@@ -100,6 +90,8 @@ if not status_ok then
 end
 
 
+-- Telescope config
+--------------
 require('telescope').setup {
   extensions = {
     fzf = {
